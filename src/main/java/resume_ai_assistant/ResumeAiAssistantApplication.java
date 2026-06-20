b99a94c4606ab6b05cdb1dfbc7f32b60ad93d86d
Nidhi <rahangdalenidhi06@gmail.com>
2026-06-20 13:22:54 +0530

    debug env

--- src/main/java/resume_ai_assistant/ResumeAiAssistantApplication.java
+++ src/main/java/resume_ai_assistant/ResumeAiAssistantApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ResumeAiAssistantApplication {
 
-	public static void main(String[] args) {SpringApplication.run(ResumeAiAssistantApplication.class, args);
-	}
+	public static void main(String[] args) {
 
+		System.out.println("DB_URL = " + System.getenv("DB_URL"));
+
+		SpringApplication.run(ResumeAiAssistantApplication.class, args);
+	}
 }
 
